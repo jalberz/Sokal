ed1fd8134d0dce4efa8ba79799fca747ad44f65f
Jacob Albers <jacobleealbers@gmail.com>
2017-03-28 23:20:28 -0500

    added getURLs

--- Suck.elm
+++ Suck.elm
@@ -1,6 +1,7 @@
 module Suck exposing (..)
 
 import HTTP
+import Json.Decode
 import List
 import Html exposing (..)
 
@@ -16,10 +17,44 @@ init : (Model, Cmd.msg)
 init = (Model [], Cmd.none)
 
 
+-- UPDATE
+
+type Msg = Suck (Result Http.Error String) | Spew
+
+update : Msg -> Model -> (Model, Cmd.Msg)
+update msg model =
+  case msg of
+    Suck -> (model, getURLs "urls.txt")
+    Spew -> (model, msg)
+
+getURLs : String -> Cmd Msg
+getURLs file =
+  let
+    urls =
+      "http://cmsc-22311.cs.uchicago.edu/2015/Labs/Lab-01/urls.txt"
+
+    request =
+      Http.get url decodeUrl
+  in
+    Http.send Suck request
+
+decodeUrl : Decode.Decoder String
+decodeUrl =
+  Decode.at ["data", "url"] Decode.string
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [text model.topic]
+        , img [src model.gifUrl] []
+        , button [ onClick MorePlease ] [ text "Harvest" ]
+        ]
+
+
 {-
 executeSuck opens a file, takes the urls from it and siphons the text, and 
 subsequently removes HTML tags from it. It calls upon makeModel to make the
 text into a frequency sorted mapping that is written to sokal.model
 -}
-
-executeSuck : 
